ffb8610e3b9a2de329185c7e14cf230087459906
eddieurbina <urbinaa123@gmail.com>
2018-12-26 05:27:55 -0600

    Create 2-Input-Text-From-User.c

---
+++ 2-Input-Text-From-User.c
@@ -0,0 +1,11 @@
+// Programa que requiere que el usuario ingrese 2 números enteros y realice una suma con ellos. 
+
+int main() {
+
+  //Completa el código faltante.
+  int numeroUno = 0; 
+  
+  printf("Ingrese el primer numero: ");
+  scanf("% ", numeroUno);
+ 
+}
