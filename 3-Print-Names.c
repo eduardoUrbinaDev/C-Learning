c92fc9825ee29f157df28c98ffb6a871846e5822
eddieurbina <urbinaa123@gmail.com>
2019-02-28 10:40:29 -0600

    Create 3-Print-Names.c

---
+++ 3-Print-Names.c
@@ -0,0 +1,11 @@
+/*
+* Instrucciones: Corrige y completa el código dado de tal manera 
+* que imprima los nombres de 3 familiares cercanos. 
+*/
+
+#include<stdio.h>
+
+int main() {
+  print("Los nombres de 3 familiares cercanos son: \n")
+  return0; 
+}
