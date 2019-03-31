42e5ba02166d956231bdfa92161e7e7af10ee0f4
eduardoUrbinaDev <47988308+eduardoUrbinaDev@users.noreply.github.com>
2019-03-30 23:41:01 -0600

    Create 6-leapYear.c

---
+++ 6-leapYear.c
@@ -0,0 +1,20 @@
+/*
+* Assign @true to the boolean variable @leapYear if the integer variable @year is a
+* leap year. A year is leap year if the following conditions are satisfied:
+     1. Year is multiple of 400.
+     2. Year is multiple of 4 and not multiple of 100.
+*/
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+    bool leapYear = false;
+    int year = 0;
+
+    printf("Input the year: ");
+    scanf("%d", &year);
+    if() {
+    }
+    return 0;
+}
