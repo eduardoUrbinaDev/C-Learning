c0da9939fa941fffd765807e05bcbee37efa02ec
eduardoUrbinaDev <47988308+eduardoUrbinaDev@users.noreply.github.com>
2019-03-30 23:16:38 -0600

    If exercise

---
+++ If-oddNumber.c
@@ -0,0 +1,18 @@
+/*
+* Si la variable currentNumber es impar, cambia su valor para que sea 3 veces
+* currentNumber + 1, de otra manera cambia su valor para que sea la mitad de
+* currentNumber
+*/
+
+#include<stdio.h>
+
+int main() {
+    int currentNumber;
+    scanf();
+    if(currentNumber % 2 != 0) {
+        printf("The number is odd, multiply it by 3 and add 1:);
+    } else {
+        printf("The number is not odd");
+    }
+    return 0;
+}
